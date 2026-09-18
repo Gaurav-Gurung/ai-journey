4bc71e80d981e40a950fde1984155b7eba79a498
Gaurav <gurunggaurav.04@gmail.com>
2026-09-18 15:52:20 +0530

    Day 1: first python script

---
+++ day1.py
@@ -0,0 +1,15 @@
+#Day 1 - My first Phython file
+#Author: Gaurav
+
+print("Day 1. My name is gaurav")
+print("I am starting my AI journey today.")
+
+print("2 + 5 =", 2 + 5)
+print("10 * 87 =", 10 * 87)
+
+first = "Hello"
+second = "World!"
+print(first + " " + second)
+
+goal = "Earn $1000+ per client from international AI work"
+print("My goal:", goal)
