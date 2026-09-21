956db20ad7a1c10d601d179d9072456d380c9861
Gaurav <gurunggaurav.04@gmail.com>
2026-09-21 17:21:25 +0530

    Day 2: My first interactive Python program

---
+++ greetings.py
@@ -0,0 +1,6 @@
+#greetings.py - my first interactive python program
+
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+
+print("Hello, " + name + "! You are " + age + " years old.")
