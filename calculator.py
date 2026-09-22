b046101ad67d3d2d69d85b9df3f5fbfd169d89c8
Gaurav <gurunggaurav.04@gmail.com>
2026-09-22 13:18:10 +0530

    Session 3: calculator.py - first working calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+#calculator.py - my first Python Calculaor
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+print("The sum of", first_number, "and", second_number, "is:", first_number + second_number)
+print("The difference of", first_number, "and", second_number, "is:", first_number - second_number)
+print("The product of", first_number, "and", second_number, "is:", first_number * second_number)
+print("The quotient of", first_number, "and", second_number, "is:", first_number / second_number)
