7fecf7ffdd81f873cbd5698145f6ede1338c14bd
Gaurav <gurunggaurav.04@gmail.com>
2026-09-22 13:29:13 +0530

    Session 3: fixed calculator labels

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-#calculator.py - my first Python Calculaor
+#calculator.py - my first Python Calculator
 
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
@@ -6,3 +6,6 @@ print("The sum of", first_number, "and", second_number, "is:", first_number + se
 print("The difference of", first_number, "and", second_number, "is:", first_number - second_number)
 print("The product of", first_number, "and", second_number, "is:", first_number * second_number)
 print("The quotient of", first_number, "and", second_number, "is:", first_number / second_number)
+print("The integer division of", first_number, "and", second_number, "is:", first_number // second_number)
+
+
